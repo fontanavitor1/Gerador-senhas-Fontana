ede097f36fd444b771d5aa942685a12715feea62
fontanavitor1 <145573605+fontanavitor1@users.noreply.github.com>
2025-08-26 08:38:00 -0300

    26

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function diminuiTamanho(){
     if(tamanhoSenha > 1){
         tamanhoSenha = tamanhoSenha - 1;
     }
-    numeroSenha.textContent=tamanhoSenha;]
+    numeroSenha.textContent=tamanhoSenha;
     geraSenha();
 }
 
