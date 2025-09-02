3d01073873bfae6a5dd2e726d1fe7dfc2ff8b35b
fontanavitor1 <145573605+fontanavitor1@users.noreply.github.com>
2025-09-02 08:05:52 -0300

    2/09

--- main.js
+++ main.js
@@ -7,23 +7,28 @@ const botoes = document.querySelectorAll('.parametro-senha__botao');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho(){
-    if(tamanhoSenha > 1){
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
         tamanhoSenha = tamanhoSenha - 1;
     }
-    numeroSenha.textContent=tamanhoSenha;
+    numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
 
-function aumentaTamanho(){
-    if(tamanhoSenha < 20){
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
         tamanhoSenha = tamanhoSenha + 1;
     }
-    numeroSenha.textContent=tamanhoSenha;
+    numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
- 
-const campoSenha = document.querySelector('#campo-senha'); 
+
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+
+for (i = 0; i < checkbox.length; i++) {
+    checkbox[i].onoclick = geraSenha;
+}
 
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
@@ -31,14 +36,29 @@ const numeros = '0123456789';
 const simbolos = '!@#$%&*?/§-';
 geraSenha();
 
+function geraSenha() {
+    let alfabeto = '';
+    if (checkbox[0].checked) {
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
 
+    if (checkbox[1].checked) {
+        alfabeto = alfabeto + letrasMinusculas;
+    }
 
-function geraSenha(){
+    if (checkbox[2].checked) {
+        alfabeto = alfabeto + numeros;
+    }
+
+    if (checkbox[3].checked) {
+        alfabeto = alfabeto + simbolos;
+    }
+
     let senha = '';
-    for (let i=0; i<tamanhoSenha; i++){
-        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+    for (let i = 0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random() * alfabeto.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
-        senha = senha + letrasMaiusculas[numeroAleatorio];
+        senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
 }
