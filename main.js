db0c8dba101fd4b1bbe3fa687d9cf621db692d53
fontanavitor1 <145573605+fontanavitor1@users.noreply.github.com>
2025-09-02 08:31:56 -0300

    02/09/25

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@#$%&*?/§-';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
+
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
@@ -23,17 +30,11 @@ function aumentaTamanho() {
     geraSenha();
 }
 
-const campoSenha = document.querySelector('#campo-senha');
-const checkbox = document.querySelectorAll('.checkbox');
-
 for (i = 0; i < checkbox.length; i++) {
     checkbox[i].onoclick = geraSenha;
 }
 
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
-const numeros = '0123456789';
-const simbolos = '!@#$%&*?/§-';
+
 geraSenha();
 
 function geraSenha() {
@@ -61,4 +62,12 @@ function geraSenha() {
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
+ classificaSenha();
+}
+
+function classificaSenha(){
+    forcaSenha.classList.remova('fraca','media','forte');
+    if(tamanhoSenha > 11){
+        forcaSenha.classList.add('forte');
+    }
 }
