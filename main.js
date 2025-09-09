2e6616dfc35731a99054a123f64eeb6b003f1ef9
fontanavitor1 <145573605+fontanavitor1@users.noreply.github.com>
2025-09-09 10:23:47 -0300

    09/09/25

--- main.js
+++ main.js
@@ -62,12 +62,17 @@ function geraSenha() {
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
- classificaSenha();
+    classificaSenha();
 }
 
-function classificaSenha(){
-    forcaSenha.classList.remova('fraca','media','forte');
-    if(tamanhoSenha > 11){
+function classificaSenha() {
+    forcaSenha.classList.remova('fraca', 'media', 'forte');
+    if (tamanhoSenha > 11) {
         forcaSenha.classList.add('forte');
+    } else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
+        forcaSenha.classList.add('media');
+    } else if (tamanhoSenha <= 5) {
+        forcaSenha.classList.add('fraca');
+
     }
 }
